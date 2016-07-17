0b07e4e702515617a101e1367140769a459a8afc
breiss <brad72287@gmail.com>
2016-07-17 16:02:27 +0000

    added take_ride method

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,25 @@
 class Ride < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :attraction
+  
+  def take_ride
+    message = "Sorry."
+    
+    if (user.tickets - attraction.tickets) < 0 
+      message << " You do not have enough tickets the #{attraction.name}."
+    end
+    
+    if user.height < attraction.min_height 
+      message << " You are not tall enough to ride the #{attraction.name}."
+    end
+    
+    if message == "Sorry."
+      user.tickets = user.tickets - attraction.tickets
+      user.happiness = user.happiness + attraction.happiness_rating
+      user.nausea = user.nausea + attraction.nausea_rating
+      user.save
+    else
+      message
+    end
+  end
 end
